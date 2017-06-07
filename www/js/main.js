615d22488ab5dc86f155efe6370ebce6aa9472b8
Krzysiek Kosecki <junnex.tay@gmail.com>
2017-06-08 00:07:57 +0200

    added new section

--- www/js/main.js
+++ www/js/main.js
@@ -1,7 +1,6 @@
 /**
  * Created by Tay on 5/28/2017.
  */
-
 // menu:
 var hamburger = document.querySelector(".menu-hamburger");
 hamburger.addEventListener("click", function () {
@@ -18,21 +17,18 @@ close.addEventListener("click", function () {
 // scrolling menu:
 $(document).on("scroll", function(){
 // or as a shorthand $(document).scroll(function(){
-    if($(document).scrollTop() > jQuery(window).height())
-    {
+    if($(document).scrollTop() > jQuery(window).height()) {
         //begin to scroll
         $("#list-progress").css("position","fixed");
         $("#list-progress").css("top",0);
     }
-    else
-    {
+    else {
         //lock it back into place
         $("#list-progress").css("position","relative");
     }
 });
 
-function isScrolledIntoView(elem)
-{
+function isScrolledIntoView(elem) {
     var docViewTop = $(window).scrollTop();
     var docViewBottom = docViewTop + $(window).height();
 
@@ -41,4 +37,21 @@ function isScrolledIntoView(elem)
 
     return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
 }
+
+// service categories:
+var photographyBtn = document.getElementById("photography-btn");
+photographyBtn.addEventListener("click", function() {
+    document.getElementById("photography-description").classList.toggle("opened");
+});
+
+var creativityBtn = document.getElementById("creativity-btn");
+creativityBtn.addEventListener("click", function() {
+    document.getElementById("creativity-description").classList.toggle("opened");
+});
+
+var web_designBtn = document.getElementById("web_design-btn");
+web_designBtn.addEventListener("click", function() {
+    document.getElementById("web_design-description").classList.toggle("opened");
+});
+
 
