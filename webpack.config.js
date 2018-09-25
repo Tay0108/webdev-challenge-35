d83c12348b35f1592b5e1f1faa5d5d1ca79fd085
Tay0108 <kjkosecki@gmail.com>
2018-09-25 15:42:37 +0200

    migrating to yarn

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 module.exports = {
   entry: './src/js/main.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
 
   devServer: {
     inline: true,
     port: 3000
   },
-  mode: 'development'
+  mode: 'development',
+
+  plugins: [
+  ]
 };
